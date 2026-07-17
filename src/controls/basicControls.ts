b02efb43dbe15178ab51c7e73b604ea33e4a876e
titan <titanio.yudista@cakrawala.ac.id>
2026-07-17 20:15:14 +0700

    Remove unused files and layers related to map visualization, including vector and raster layers, main application logic, styles, and configuration files.

--- src/controls/basicControls.ts
+++
@@ -1,8 +0,0 @@
-import { AttributionControl, Map } from "maplibre-gl";
-
-export function addAttribution(map: Map, att: string): void {
-    map.addControl(new AttributionControl({
-        compact: true,
-        customAttribution: att
-    }));
-}
